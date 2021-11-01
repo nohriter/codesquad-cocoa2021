9b9c2e41333ebf4926960da6d6d46586d493f6a6
NohJangHyuk <nohriter@gmail.com>
2021-11-01 17:29:51 +0900

    feat: 반복문 풀이

--- src/first_week/first_day/loop_example/Loop_11720.java
+++ src/first_week/first_day/loop_example/Loop_11720.java
@@ -1,2 +1,22 @@
-package first_week.first_day.loop_example;public class Loop_11720 {
+package first_week.first_day.loop_example;
+
+import java.util.Scanner;
+
+public class Loop_11720 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numberCount = Integer.parseInt(scanner.next());
+        String number = scanner.next();
+
+        String[] numbers = number.split("");
+        
+        int sum = 0;
+        
+        for(int i=0; i < numberCount; i++) {
+            sum += Integer.parseInt(numbers[i]);
+        }
+
+        System.out.println(sum);
+    }
 }
--- src/first_week/first_day/loop_example/Loop_2438.java
+++ src/first_week/first_day/loop_example/Loop_2438.java
@@ -1,2 +1,19 @@
-package first_week.first_day.loop_example;public class loop_2438 {
+package first_week.first_day.loop_example;
+
+import java.util.Scanner;
+
+public class Loop_2438 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt();
+
+        for (int i = 0; i < number; i++) {
+            for (int j = 0; j < i+1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
 }
